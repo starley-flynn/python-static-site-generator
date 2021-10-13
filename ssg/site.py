9447a91cf8e4ec0f5db20372c9484ec05e451c84
Starley Flynn <starley.flynn@salesforce.com>
2021-10-13 16:02:32 -0700

    Site Class Steps 1-10

---
+++ ssg/site.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import typer
+from ssg.site import Site
+
+class Site:
+
+    def __init__(self, source, dest):
+        self.source = Path(source)
+        self.dest = Path(dest)
+
+    def create_dir(self, path):
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents = True, exist_ok = True)
+
+    def build(self):
+        self.dest.mkdir(parents = True, exist_ok = True)
+        for path in self.source.rglob("*"):
+            if path.is_dir():
+                self.create__dir(path)
+            elif path.is_file():
+                self.run_parser(path)
+
+    def main(self, source = "content", dest = "dist"):
+        config = {"source" : source, "dest": dest}
+        Site(**config).build()
+
+    typer.run(main)
