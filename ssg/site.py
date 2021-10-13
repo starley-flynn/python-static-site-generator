444646c6d534204b310bfe8623f2086c48b0859b
Starley Flynn <starley.flynn@salesforce.com>
2021-10-13 16:21:29 -0700

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -13,7 +13,7 @@ class Site:
         directory.mkdir(parents = True, exist_ok = True)
 
     def build(self):
-        self.dest.mkdir(parents = True, exist_ok = True)
+        self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
