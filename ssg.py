55628705ece1e60b34cfa7f52e9287613bf17e94
Starley Flynn <starley.flynn@salesforce.com>
2021-10-18 15:23:25 -0700

    Update ssg.py

--- ssg.py
+++ ssg.py
@@ -1,12 +1,16 @@
 import typer
 from ssg.site import Site
 
-def main(source = "content", dest = "dist"):
-        config = {
+import ssg.parsers
+
+
+def main(source="content", dest="dist"):
+    config = {
         "source": source,
         "dest": dest,
         "parsers": [ssg.parsers.ResourceParser(),],
-        }
+    }
     Site(**config).build()
+
 
 typer.run(main)
